0f44074b168b3b92dae0ac785661dff6c877cacb
Albina <albina.melsitova@mail.ru>
2022-12-25 23:31:13 +0300

    FindMedianSortedArrays accepted

--- src/leedCode/FindMedianSortedArrays.java
+++ src/leedCode/FindMedianSortedArrays.java
@@ -1,5 +1,7 @@
 package leedCode;
 
+import java.util.Arrays;
+
 /**
  *Учитывая два отсортированных массива nums1и nums2размер mи nсоответственно, вернуть медиану двух отсортированных массивов.
  *
@@ -18,6 +20,52 @@ package leedCode;
  */
 public class FindMedianSortedArrays {
     public static void main(String[] args) {
+        double mediana = 0.0;
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4 };
+        int[] result = findMedianSortedArrays(nums1, nums2);
+        int index = result.length / 2;
+        System.out.println(result[index]);
+        if (result.length % 2 == 0) {
+            mediana = (result[index - 1] + result[index]) / 2.0;
+        } else {
+            mediana = result[index];
+        }
 
+        System.out.println(mediana);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length + nums2.length];
+        int index1 = 0;
+        int index2 = 0;
+        int indexRes = 0;
+        int m = nums1.length;
+        int n  = nums2.length;
+        int length = m + n;
+
+        while (indexRes < length ) {
+            if(index1 >= nums1.length) {
+                result[indexRes] = nums2[index2];
+                index2++;
+            } else
+            if (index2 >= nums2.length) {
+                result[indexRes] = nums1[index1];
+                index1++;
+            } else
+            if ( nums1[index1] < nums2[index2]) {
+                if(index1 < nums1.length) {
+                    result[indexRes] = nums1[index1];
+                    index1++;
+                }
+            } else {
+                if (index2 < nums2.length) {
+                    result[indexRes] = nums2[index2];
+                    index2++;
+                }
+            }
+            indexRes++;
+        }
+        return result;
     }
 }
